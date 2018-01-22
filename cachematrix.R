26c7167dfc7abca70437cee0d20ccd6deece0688
ssd <deshmukh.sudarshan@gmail.com>
2018-01-22 13:12:59 +0530

    This is second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This funtion avoid recomputations for static vectors by putting the computed data into 
+## cache. 
+## It checks ,whether data is already computed and present in cache . If YES, then dont 
+## compute again else otherwise.
 
-## Write a short comment describing this function
+## creates a special "vector", which is really a list containing a different functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## calculates the mean of the special "vector" created with the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
